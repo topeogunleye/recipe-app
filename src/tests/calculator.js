6544fd54628999137988337037a946e133ae0292
emmanuel <gbengacode>
2021-12-20 15:43:22 +0100

    Fix linter errors

--- src/tests/calculator.js
+++ src/tests/calculator.js
@@ -1,22 +1,22 @@
 export default class Calculator {
-  constructor(first, second){
+  constructor(first, second) {
     this.first = first;
     this.second = second;
   }
 
   add() {
-    return this.first + this.second
+    return this.first + this.second;
   }
 
-  subtract(){
-    return this.second - this.first
+  subtract() {
+    return this.second - this.first;
   }
 
-  divide(){
-    return this.second/this.first
+  divide() {
+    return this.second / this.first;
   }
 
   multiply() {
-    return this.first*this.second
+    return this.first * this.second;
   }
 }
