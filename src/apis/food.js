483d926af9c5664af1ac4157582507175bc2e8ee
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-12-28 13:57:11 +0100

    Move paginate function to index.js and import paginate function into food.js

--- src/apis/food.js
+++ src/apis/food.js
@@ -1,8 +1,9 @@
+import { paginate } from '../index.js';
+
 let meals = [];
 let currentPosts = [];
 let currentPage = 1;
 const postsPerPage = 10;
-
 
 // Get current posts
 let indexOfLastPost = currentPage * postsPerPage;
@@ -15,43 +16,42 @@ const getMealsLength = (meals, currentPosts) => {
   const totalMeals = meals.length;
   showingNumbers.innerHTML = `  <p class="text-sm text-gray-700">
   Showing
-  <span class="font-medium">1</span>
+  <span class="font-medium">${indexOfFirstPost + 1}</span>
   to
-  <span class="font-medium">${currentPosts.length}</span>
+  <span class="font-medium">${currentPosts.length >= 10 ? indexOfLastPost : totalMeals}</span>
   of
   <span class="font-medium">${totalMeals}</span>
   meals
 </p>`;
-  mealsCounter.innerHTML = `<p class="text-sm font-bold text-gray-700">${currentPosts.length} meals</p>`;
+  mealsCounter.innerHTML = `<p class="text-sm font-bold text-gray-700">${totalMeals} meals</p>`;
 };
 
 export const getCurrentPosts = async () => {
   currentPosts = meals.slice(indexOfFirstPost, indexOfLastPost);
-  getMealsLength(meals, currentPosts)
   return currentPosts;
 };
 
 export const searchFood = async (term) => {
   const response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`,
   );
   const data = await response.json();
-  meals = data.meals;  
+  meals = data.meals;
   getMealsLength(meals, currentPosts);
   currentPosts = getCurrentPosts();
-  paginate()
+  paginate(meals, currentPage);
   return currentPosts;
 };
 
 export const defaultFood = async () => {
   const response = await fetch(
-    'https://www.themealdb.com/api/json/v1/1/search.php?s=chicken'
+    'https://www.themealdb.com/api/json/v1/1/search.php?s=',
   );
   const data = await response.json();
   meals = data.meals;
   currentPosts = getCurrentPosts();
   getMealsLength(meals, currentPosts);
-  paginate();
+  paginate(meals, currentPage);
   return getCurrentPosts();
 };
 
@@ -62,6 +62,7 @@ export const handlePrevBtn = () => {
   indexOfLastPost = currentPage * postsPerPage;
   indexOfFirstPost = indexOfLastPost - postsPerPage;
   getCurrentPosts();
+  getMealsLength(meals, currentPosts);
 };
 
 export const handleNextBtn = () => {
@@ -72,6 +73,7 @@ export const handleNextBtn = () => {
   indexOfLastPost = currentPage * postsPerPage;
   indexOfFirstPost = indexOfLastPost - postsPerPage;
   getCurrentPosts();
+  getMealsLength(meals, currentPosts);
 };
 
 export const handlePageBtn = (page) => {
@@ -79,29 +81,5 @@ export const handlePageBtn = (page) => {
   indexOfLastPost = currentPage * postsPerPage;
   indexOfFirstPost = indexOfLastPost - postsPerPage;
   getCurrentPosts();
-};
-
-const paginate = () => {
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(meals.length / postsPerPage); i += 1) {
-    pageNumbers.push(i);
-  }
-  const page = document.querySelector('.page');
-  page.innerHTML = '';
-  const ul = document.createElement('ul');
-  ul.classList.add('inline-flex');
-  pageNumbers.forEach((number) => {
-    const li = document.createElement('li');
-    li.classList.add('page-item');
-    if (number === currentPage) {
-      li.classList.add('active');
-    }
-    li.innerHTML = `<a class="page-link bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium" href="#">${number}</a>`;
-    li.addEventListener('click', () => {
-      handlePageBtn(number);
-    });
-
-    ul.appendChild(li);
-  });
-  page.appendChild(ul);
+  getMealsLength(meals, currentPosts);
 };
