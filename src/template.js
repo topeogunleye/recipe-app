d45d4c5b4047c2eb94b3d7519990ef64756c9141
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-12-30 09:32:52 +0100

    Prettify template.js

--- src/template.js
+++ src/template.js
@@ -1,4 +1,4 @@
-import { getMealsLength } from "./apis/food";
+import { getMealsLength } from './apis/food';
 
 export default async (searchFood, mealsEl, resultHeading) => {
   // Change HTML
@@ -12,21 +12,21 @@ export default async (searchFood, mealsEl, resultHeading) => {
           (meal) => `
             <div class="meal">
               <img src="${meal.strMealThumb}" alt="${
-  meal.strMeal
-}" class="meal-img"/>
+            meal.strMeal
+          }" class="meal-img"/>
               <div class="m-4 meal-info">
               <div class="title-likes">
                 <span class="font-bold meal-title">${
-  meal.strMeal.length > 14
-    ? `${meal.strMeal.slice(0, 14)}...`
-    : meal.strMeal
-} </span>
+                  meal.strMeal.length > 14
+                    ? `${meal.strMeal.slice(0, 14)}...`
+                    : meal.strMeal
+                } </span>
                 <span class="likes"></span>
               </div>
               <div class="comment-like">
                 <button class="meal-btn" data-mealID="${
-  meal.idMeal
-}">Comments<svg class="meal-svg" width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style=""><path d="M0 0L3 3L0 6"></path></svg>
+                  meal.idMeal
+                }">Comments<svg class="meal-svg" width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style=""><path d="M0 0L3 3L0 6"></path></svg>
                 </button>
                 <i class="fas fa-heart" id= ${meal.idMeal}></i>
       
@@ -35,7 +35,7 @@ export default async (searchFood, mealsEl, resultHeading) => {
             </div>
             </div>
       
-            `,
+            `
         )
         .join('');
       getMealsLength();
